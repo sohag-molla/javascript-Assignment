4759ff91f3486c9780c43da3a818ba971d88a8d7
sohag-molla <pcgamers579@gmasil.com>
2024-05-31 16:37:07 +0600

    javascript first project done

---
+++ plmsolving.js
@@ -0,0 +1,100 @@
+//1.Variable Create//
+
+let carName = "Volvo";
+console.log(carName)
+
+//2.Variable Names//
+
+let firstName = "John", lastName = "Doe",age = 35;
+
+
+let ... = "John";  // write variable name: firstName.
+let lastName = "..." //write variable values: Doe.
+let ... = ... ; // write variable name and values both: firstName + = + lastName.
+
+// console.log(firstName)
+
+
+ 3.assigment operator//
+
+x = 10;
+y = 5;
+
+let mult = x*y; // x = 50//
+
+//console.log(mult)
+
+//4.Data Type//
+
+let length = 16; // data type:Number.
+let lastName = "Johnson"; // data type:String.
+
+const x = {
+  firstName: "John",  
+  lastName: "Doe"};// data type:object.
+
+
+//5.Function Alert//
+
+
+ function myFunction() {
+  alert("Hello World!");}
+
+Call the Function:myFunction();
+
+//6.Object Alert//
+
+var person = {
+  name:"John",
+  age:50,
+ };
+ alert(person.name + " is " + person.age);
+
+
+//7.Button element//
+
+function clickOn(){
+  alert("Try to fix it!")};
+
+
+//8.Array //
+
+const cars = ["Volvo", "Jeep", "Mercedes"];
+alert(cars.length)
+
+
+//change item -,Ford,//
+
+const Brand = ["Volvo", "Jeep", "Mercedes"]; 
+Brand[0] = "Ford";
+
+//console.log(Brand)
+
+
+
+//9.Math//
+
+let random = Math.random();
+console.log(random) // Ans:....
+
+
+let largestNumber = Math.max(10,20);
+console.log(largestNumber) // Ans:20
+
+let squareRoot = Math.sqrt(9)
+console.log(squareRoot) // Ans:3
+
+
+//10.comparison operator//
+
+let age = 19;
+
+if(age < 18){
+  alert("Too young")
+}
+else {
+  alert("Old enough")
+}
+
+
+
